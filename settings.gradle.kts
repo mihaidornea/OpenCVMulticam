ddf2e701d9a75ca18f50de2cf90ee7ec1551417e
mdornea <mdornea97@gmail.com>
2023-10-02 21:41:36 +0300

    Initial Sources Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://raw.github.com/saki4510t/libcommon/master/repository/") }
+        maven { url = uri("https://jitpack.io") }
+        jcenter()
+    }
+}
+
+include(":app", ":renderscript-toolkit", ":OpenCV")
+rootProject.name = "OpenCVApp"
