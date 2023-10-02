06904378c8e0a27ac25b1f9dd81dc6eaf31175d0
mdornea <mdornea97@gmail.com>
2023-10-02 22:17:05 +0300

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # OpenCVMulticam
+
+### Android application that renders the feeds from two cameras (one internal via Camera2 API, one external via UVCCamera) and processes them using OpenCV C++ and renders a cube on top of them using OpenGL ES C++
+
+## Project Structure
+
+# App
+
+Application module that consists of the following packages:
+
+- application: contains the application class and koin injection modules
+- shared: contains the base classes for viewmodels and fragments, extensions and utilities
+- presentation: Consists of the Main Activity and it's associated viewmodel and the camera fragment
+  that uses two viewmodels. CameraViewModel handles internal camera operations and processing, while
+  ExternalCameraViewModel handles the external camera operations. The external camera permission
+  requests and basic commands are handled by CameraFragment() which from the AndroidUSBCamera
+  library and is based on UVCCamera.
+
+# App/cpp
+
+C++ Files that are used for OpenCV processing and OpenGL rendering.
+
+- native-lib.cpp implements JNI calls and operations needed for processing frames from the cameras
+  using OpenCV: rotations and color space conversions.
+- openGLJni.cpp implements the JNI calls required for rendering a cube with OpenGL which will then
+  be displayed on a surface in the fragment
+- renderer.cpp: implements the OpenGL rendering functionality and displays the result on a
+  AndroidNativeWindow which is the SurfaceView passed from the CameraFragment 
--- app/src/main/java/com/mihaidornea/opencvapp/shared/base/BaseFragment.kt
+++ app/src/main/java/com/mihaidornea/opencvapp/shared/base/BaseFragment.kt
@@ -19,66 +19,6 @@ import com.mihaidornea.opencvapp.shared.base.BaseViewModel.BaseCommand.ShowToast
 import com.mihaidornea.opencvapp.shared.base.BaseViewModel.BaseCommand.ShowToastById
 import com.mihaidornea.opencvapp.shared.utils.extensions.snackBar
 
-abstract class BaseFragment<BINDING : ViewDataBinding, VIEW_MODEL : BaseViewModel>(
-    @LayoutRes private val layoutRes: Int
-) : Fragment() {
-
-    protected var binding: BINDING? = null
-        private set
-    protected abstract val viewModel: VIEW_MODEL
-
-
-    @CallSuper
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ) = DataBindingUtil.inflate<BINDING>(
-        layoutInflater,
-        layoutRes,
-        null,
-        false
-    ).also {
-        it.lifecycleOwner = viewLifecycleOwner
-        it.setVariable(BR.viewModel, viewModel)
-        binding = it
-    }.root
-
-    @CallSuper
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setupViews()
-        observeBaseCommands()
-    }
-
-    private fun observeBaseCommands() {
-        viewModel.baseCmd.observe(viewLifecycleOwner) { command ->
-            when (command) {
-                is ShowToastById ->
-                    Toast.makeText(context ?: return@observe, command.stringId, Toast.LENGTH_LONG)
-                        .show()
-
-                is ShowToast ->
-                    Toast.makeText(context ?: return@observe, command.message, Toast.LENGTH_LONG)
-                        .show()
-
-                is ShowSnackbarById -> snackBar(
-                    message = getString(command.stringId),
-                    actionText = command.actionStringId?.let { getString (it) },
-                    action = command.action
-                )
-                is ShowSnackbar -> snackBar(
-                    message = command.message,
-                    actionText = command.actionString,
-                    action = command.action
-                )
-            }
-        }
-    }
-
-    abstract fun setupViews()
-}
-
 abstract class BaseCameraFragment<BINDING : ViewDataBinding, VIEW_MODEL : BaseViewModel>(
 @LayoutRes private val layoutRes: Int
 ) : MultiCameraFragment() {
